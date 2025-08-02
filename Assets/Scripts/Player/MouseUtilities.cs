b31a4ad05e05e8f0100a3592c51ca140975eee0c
Fabian Mauz <fabian66@freenet.de>
2025-08-02 15:49:10 +0200

    feat: add moving and look direction

---
+++ Assets/Scripts/Player/MouseUtilities.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MouseUtilities : MonoBehaviour
+{
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    public Vector2 getMouseDirection(Vector2 origin)
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Vector2 mouseWorldPos = cam.ScreenToWorldPoint(mouseScreenPos);
+        return mouseWorldPos - origin;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-   [SerializeField] private float moveSpeed;
-   [Header("Components")]
-   [SerializeField] private Rigidbody2D rig;
-   [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float moveSpeed;
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rig;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private MouseUtilities mouseUtils;
 
-   private Vector2 moveInput;
+    private Vector2 moveInput;
 
+
+    void Update()
+    {
+        Vector2 mousePosition = mouseUtils.getMouseDirection(this.transform.position);
+        spriteRenderer.flipX = mousePosition.x < 0;
+    }
     void FixedUpdate()
     {
-        Vector2 velocity = moveInput*moveSpeed;
-        rig.linearVelocity=velocity;
+        Vector2 velocity = moveInput * moveSpeed;
+        rig.linearVelocity = velocity;
+    }
+
+    public void onMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
