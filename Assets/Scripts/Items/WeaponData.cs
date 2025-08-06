f5610b9ae4c7dc13bf90743ab1eeb23dda88fb90
Fabian Mauz <fabian66@freenet.de>
2025-08-06 23:39:33 +0200

    feat: add basic item data

---
+++ Assets/Scripts/Items/WeaponData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon Data", menuName = "Item/Weapon")]
+public class WeaponData : ItemData
+{
+    [Header("Weapon Data")]
+    public float maxDamage;
+    public float minDamage;
+    public float range;
+    public float attackRate;
+
+    public GameObject projectilePrefab;
+
+    public void Trigger(
+        Vector3 spawnPosition,
+        Quaternion spawnRotation,
+        Character.Team team)
+    {
+
+    }
+}
