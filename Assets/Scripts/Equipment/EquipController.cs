f6c0cf4fe65984ca17d87e65920719d81f01222d
Fabian Mauz <fabian66@freenet.de>
2025-08-07 11:30:18 +0200

    feat: started equip controller

--- Assets/Scripts/Equipment/EquipController.cs
+++ Assets/Scripts/Equipment/EquipController.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class EquipController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private EquipItem leftHandItem;
+    private GameObject leftHandItemGo;
 
-    // Update is called once per frame
+    private bool useLeftHand;
+
+    [Header("Components")]
+    [SerializeField] private Transform leftHandItemOrigin;
+    [SerializeField] private MouseUtilities mouseUtils;
+
     void Update()
     {
-        
+        Vector2 mouseDir = mouseUtils.getMouseDirection(transform.position);
+        transform.up = mouseDir;
+
+        if (hasLeftHAndItemEquiped())
+        {
+            if (useLeftHand && !EventSystem.current.IsPointerOverGameObject())
+            {
+                leftHandItem.onUse(); 
+            }
+        }
+    }
+
+    public void EquipLeftHand(ItemData item)
+    {
+        if (hasLeftHAndItemEquiped())
+        {
+            UnEquipLeftHand();
+        }
+        leftHandItemGo = Instantiate(item.EquipPrefab, leftHandItemOrigin);
+        leftHandItem = leftHandItemGo.GetComponent<EquipItem>();
+
+    }
+
+    public void UnEquipLeftHand()
+    {
+        if (leftHandItemGo != null)
+        {
+            Destroy(leftHandItemGo);
+        }
+        leftHandItem = null;
+    }
+
+    public bool hasLeftHAndItemEquiped()
+    {
+        return leftHandItem != null;
+    }
+
+    public void OnUseInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            useLeftHand = true;
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            useLeftHand = false;
+        }
     }
 }
