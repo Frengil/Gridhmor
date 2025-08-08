9e6a837765f963eb2a07242f3bd814b79ae98013
Fabian Mauz <fabian66@freenet.de>
2025-08-08 14:22:36 +0200

    feat: started inventory logic

--- Assets/Scripts/UI/Inventory/Inventory.cs
+++ Assets/Scripts/UI/Inventory/Inventory.cs
@@ -2,15 +2,82 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private ItemData[] starterItems;
+    [SerializeField] private int inventorySize;
+
+    private ItemSlot[] slots;
+
+    public InventoryUI inventoryUI;
+
+    public static Inventory instance;
+
+    void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        slots = new ItemSlot[inventorySize];
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = new ItemSlot();
+        }
+
+        for (int i = 0; i < starterItems.Length; i++)
+        {
+            AddItem(starterItems[i]);
+        }
+    }
+
+    public void AddItem(ItemData item)
+    {
+
+    }
+
+    public void RemoveItem(ItemSlot slot)
+    {
+
+    }
+
+    ItemSlot findSlotWithItem(ItemData item)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i] != null && slots[i].item == item && slots[i].item.MaxStackSize <= slots[i].quantity)
+            {
+                return slots[i];
+            }
+        }
+        return null;
+    }
+
+    ItemSlot getEmptySlot()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i] == null)
+            {
+                return slots[i];
+            }
+        }
+        return null;
+    }
+
+    public void useItem(ItemSlot slot)
+    {
+
+    }
+
+    public bool hasItem(ItemData item)
+    {
+        return false;
     }
 }
--- Assets/Scripts/UI/Inventory/ItemSlot.cs
+++ Assets/Scripts/UI/Inventory/ItemSlot.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public ItemData item;
+    public int quantity;
 }
